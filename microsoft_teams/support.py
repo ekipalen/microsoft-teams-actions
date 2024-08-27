08b74ca0669658236a75cfc19d541de1dc15eb29
Erik Palen <84192057+ekipalen@users.noreply.github.com>
2024-08-27 18:38:06 +0300

    Small updates

--- microsoft_teams/support.py
+++ microsoft_teams/support.py
@@ -21,13 +21,25 @@ def parse_channel_messages(response_data: dict) -> list:
     parsed_messages = []
 
     for message in response_data.get("value", []):
-        parsed_message = {
-            "id": message.get("id"),
-            "createdDateTime": message.get("createdDateTime"),
-            "from": message.get("from", {}).get("user", {}).get("displayName"),
-            "sender_id": message.get("from", {}).get("user", {}).get("id"),
-            "content": message.get("body", {}).get("content"),
-        }
+        # Check if the message is a system event message
+        if message.get("messageType") == "systemEventMessage":
+            # Handle system event message
+            parsed_message = {
+                "id": message.get("id"),
+                "createdDateTime": message.get("createdDateTime"),
+                "eventDetail": message.get("eventDetail"),
+                "content": "[System Event Message]",
+            }
+        else:
+            # Handle normal message
+            parsed_message = {
+                "id": message.get("id"),
+                "createdDateTime": message.get("createdDateTime"),
+                "from": message.get("from", {}).get("user", {}).get("displayName"),
+                "sender_id": message.get("from", {}).get("user", {}).get("id"),
+                "content": message.get("body", {}).get("content"),
+            }
+
         parsed_messages.append(parsed_message)
 
     return parsed_messages
